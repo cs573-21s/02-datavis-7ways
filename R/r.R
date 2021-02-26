24a6d34349962e73d5f7b1db761f217649337d2b
evelyntrvn <evelyntran235@gmail.com>
2021-02-26 14:35:28 -0500

    Edits with R and naming folders

--- R/r.R
+++ R/r.R
@@ -1,7 +1,7 @@
-# install.packages("ggplot2")
+library(tidyverse)
 library(ggplot2)
 
-df <- read_csv("GitHub/02-datavis-7ways/cars-sample.csv")
+df <- read_csv("cars-sample.csv")
 plt <- ggplot(data=df, aes(x=Weight, y=MPG)) 
 plt <- plt + geom_point(aes(size=Weight, color=Manufacturer), alpha=0.5)
 
